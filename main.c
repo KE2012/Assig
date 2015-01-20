50cbbe5a0a429893d2053e5c44aa8c3e1833db21
KE2012 <ke604@york.ac.uk>
2015-01-20 12:25:42 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+
+int G;
+
+int main() {
+
+printf("Year 2 Assignment: T9 Predictive Text\n\nStudent ID: Y1482466\n\n");
+printf("Press any key to continue.\n\n");
+getch();
+system("cls");
+printf("This T9 Predictive Text program uses keys:\nT,Y,U\n");
+printf("G,H,J\n");
+printf("B,N,M\n");
+printf("SPACE\n\n");
+printf("To Operate, T = 1, SPACE = 0\n\n");
+printf("Press '`' at any time to show these instructions");
+getch();
+system("cls");
+
+
+
+}
+
+end;
